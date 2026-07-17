66c5b64cbdf91d88b55dc94e8787626af6b9ea38
abdou7427 <abdouscd44@gmail.com>
2026-07-17 17:33:39 +0100

    Add notification handling methods to HybridNotificationService
    Implemented notification handling methods with placeholders for future code.

--- app/src/main/java/com/termux/app/HybridNotificationService.java
+++ app/src/main/java/com/termux/app/HybridNotificationService.java
@@ -1,12 +1,14 @@
 package com.termux.app;
-
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
 public class HybridNotificationService extends NotificationListenerService {
     @Override
-    public void onNotificationPosted(StatusBarNotification sbn) {}
-
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        // سيتم إضافة كود قراءة الإشعارات لاحقاً
+    }
     @Override
-    public void onNotificationRemoved(StatusBarNotification sbn) {}
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        // Required method
+    }
 }
